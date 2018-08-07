aa270b7955b5a2a89a93b22d72dbe2bfe6b2cb73
smiley-coyote <nick.a.alex@gmail.com>
2018-08-07 17:04:17 -0400

    code works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,8 +6,38 @@ module.exports = function (app){
    })
    app.post('/api/survey', function (req, res){
       var userResults = req.body;
-      console.log(userResults);
+      userResults = userResults.userData;
+      var userNumbers = userResults.map(function(x) { return parseInt(x, 10) })
+     console.log(userNumbers);
+     
+     var totalResults = [];
+     for(x=0; x<movieData.length; x++){
+      var newResults = 0;
+      var newArray = [];
+      var movieMatch;
+     for(i=0;i<movieData[x].results.length;i++){
+        
+        var newNumber;
+        newNumber = userNumbers[i] - movieData[x].results[i]
+        if(newNumber<0){
+        newNumber *= -1
+        }
+        newArray.push(newNumber);
+     }
+     for(y=0;y<newArray.length;y++){
+      newResults = newResults + newArray[y];
       
-      
+     }
+     totalResults.push(newResults);
+   }
+     console.log(totalResults)
+     var lowestNumber = Math.min(...totalResults);
+     for(z=0;z<totalResults.length;z++){
+        if(lowestNumber === totalResults[z]){
+         movieMatch = movieData[z].title;
+        }
+     }
+     console.log(movieMatch);
    })
+
 }
